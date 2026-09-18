4ef5705a77d84ad9713f51d512e3b679c5f7a003
Jeyakumar <jeyaasok@gmail.com>
2026-09-19 00:03:54 +0530

    changes

--- src/app/pages/contact/admin/item/item.component.ts
+++ src/app/pages/contact/admin/item/item.component.ts
@@ -3,6 +3,10 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmComponent } from '@bracezin/components/confirm/confirm.component';
+import { PasswordChangeComponent } from '../../card/password-change/password-change.component';
+
 import { Address, AdminService, AddressService, User, AddressModel, PayoutService, UserService } from 'src/@bracezin/_dbShare';
 
 @Component({
@@ -29,7 +33,8 @@ export class ItemComponent implements OnInit {
     public adminService: AdminService,
     public userService: UserService,
     public addressService: AddressService,
-    public payoutService: PayoutService) {
+    public payoutService: PayoutService,
+		private matDialog: MatDialog) {
       this.dataInit();
   }
 
@@ -76,5 +81,82 @@ export class ItemComponent implements OnInit {
     this.sideView = 'payment-form';
     this.sideBar?.toggle();
   }
+
+  changePassword(): void {
+      if(this.admin && this.admin.id) {
+        let dialogRef = this.matDialog.open(PasswordChangeComponent, {
+          disableClose: false,
+          width: '400px'
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true && result.password && result.password.length > 0) {
+            this.adminService.storeByPost('update-password', {id: this.admin.id, user_id: this.admin.id, password: result.password});
+          }
+        });
+      }
+    }
+  
+    doChangePassword(): void {
+      if (this.userService.permissionMatch(['manage admin'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Change Password  !!!',
+            message: 'Are you sure want to Ask the User to change the password forcefully ?',
+            item: this.admin
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'do_change_password': 1};
+            this.adminService.update(this.admin.id, updateData);
+          }
+        });
+      }
+    }
+  
+    doVerifyEmail(): void {
+      if (this.userService.permissionMatch(['manage admin'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Verfiy Email  !!!',
+            message: 'Are you sure want to Ask the User to verify the Email forcefully ?',
+            item: this.admin
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'is_email_verified': 0, email_verified_at: null };
+            this.adminService.update(this.admin.id, updateData);
+          }
+        });
+      }
+    }
+  
+    doVerifyMobile(): void {
+      if (this.userService.permissionMatch(['manage admin'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Verfiy Mobile  !!!',
+            message: 'Are you sure want to Ask the User to verify the Mobile forcefully ?',
+            item: this.admin
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'is_mobile_verified': 0, mobile_verified_at: null };
+            this.adminService.update(this.admin.id, updateData);
+          }
+        });
+      }
+    }
   
 }
--- src/app/pages/contact/company/item/item.component.ts
+++ src/app/pages/contact/company/item/item.component.ts
@@ -3,6 +3,10 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmComponent } from '@bracezin/components/confirm/confirm.component';
+import { PasswordChangeComponent } from '../../card/password-change/password-change.component';
+
 import { Address, CompanyService, AddressService, User, AddressModel, PayoutService, UserService } from 'src/@bracezin/_dbShare';
 
 @Component({
@@ -29,7 +33,8 @@ export class ItemComponent implements OnInit {
     public companyService: CompanyService,
     public userService: UserService,
     public addressService: AddressService,
-    public payoutService: PayoutService) {
+    public payoutService: PayoutService,
+		private matDialog: MatDialog) {
       this.dataInit();
   }
 
@@ -76,5 +81,82 @@ export class ItemComponent implements OnInit {
     this.sideView = 'payment-form';
     this.sideBar?.toggle();
   }
+
+  changePassword(): void {
+      if(this.company && this.company.id) {
+        let dialogRef = this.matDialog.open(PasswordChangeComponent, {
+          disableClose: false,
+          width: '400px'
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true && result.password && result.password.length > 0) {
+            this.companyService.storeByPost('update-password', {id: this.company.id, user_id: this.company.id, password: result.password});
+          }
+        });
+      }
+    }
+  
+    doChangePassword(): void {
+      if (this.userService.permissionMatch(['manage company'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Change Password  !!!',
+            message: 'Are you sure want to Ask the User to change the password forcefully ?',
+            item: this.company
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'do_change_password': 1};
+            this.companyService.update(this.company.id, updateData);
+          }
+        });
+      }
+    }
+  
+    doVerifyEmail(): void {
+      if (this.userService.permissionMatch(['manage company'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Verfiy Email  !!!',
+            message: 'Are you sure want to Ask the User to verify the Email forcefully ?',
+            item: this.company
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'is_email_verified': 0, email_verified_at: null };
+            this.companyService.update(this.company.id, updateData);
+          }
+        });
+      }
+    }
+  
+    doVerifyMobile(): void {
+      if (this.userService.permissionMatch(['manage company'])) {
+        let dialogRef = this.matDialog.open(ConfirmComponent, {
+          disableClose: false,
+          width: '600px',
+          data: {
+            type: 'info',
+            title: 'Force to Verfiy Mobile  !!!',
+            message: 'Are you sure want to Ask the User to verify the Mobile forcefully ?',
+            item: this.company
+          }
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          if (result && result.data === true) {
+            let updateData = {'is_mobile_verified': 0, mobile_verified_at: null };
+            this.companyService.update(this.company.id, updateData);
+          }
+        });
+      }
+    }
   
 }
